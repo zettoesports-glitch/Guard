6f86c5c75a52c55495e6a5db58c60c47f61e0e06
zettoesports-glitch <zettoesports@gmail.com>
2026-09-20 13:47:47 -0700

    Season52: expose Crywolf chaos-rate request

--- MuElion/MuMain-main/src/source/Network/Season52/Season52Direct.h
+++ MuElion/MuMain-main/src/source/Network/Season52/Season52Direct.h
@@ -85,6 +85,7 @@ public:
                       std::uint8_t itemSlot);
     bool SendInventoryRequest(Connection* connection);
     bool SendCrywolfInfoRequest(Connection* connection);
+    bool SendCrywolfChaosRateBenefitRequest(Connection* connection);
     bool SendChecksumResponse(Connection* connection, std::uint32_t checksum);
     bool SendCastleSiegeStatusRequest(Connection* connection);
     bool SendCastleSiegeRegistrationStateRequest(Connection* connection);
